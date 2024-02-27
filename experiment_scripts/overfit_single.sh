db1e9a04478c8fba9cc4e6b602d6844daa56711b
David Bauer <david.bauer009@gmail.com>
2024-02-27 14:16:26 -0800

    minor

--- experiment_scripts/overfit_single.sh
+++ experiment_scripts/overfit_single.sh
@@ -1,6 +1,6 @@
 python experiment_scripts/train_single_class.py \
-    --data_root /home/davbauer/datasets/light_field_networks/cars/cars_train.hdf5 \
-    --experiment_name overfit_single \
+    --data_root $HOME/datasets/light_field_networks/chairs/chairs_train.hdf5 \
+    --experiment_name $1 \
     --fit_single true \
     --num_epochs 1000 \
     --steps_til_summary 25 \
